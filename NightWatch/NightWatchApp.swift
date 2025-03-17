aae69e8a99b8a472df1cb3f4d79c4da0073ea003
Brian Simmons <bsimmons01@gmail.com>
2025-03-17 14:55:58 -0700

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Brian Simmons on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
