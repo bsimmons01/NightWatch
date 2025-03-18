d1cb84c5c0188273b980c6c07d7506b904cfffe9
Brian Simmons <bsimmons01@gmail.com>
2025-03-18 13:48:35 -0700

    NavigationStacks

---
+++ NightWatch/Experiments/FirstSwiftUIGrid.swift
@@ -0,0 +1,18 @@
+//
+//  FirstSwiftUIGrid.swift
+//  NightWatch
+//
+//  Created by Brian Simmons on 3/18/25.
+//
+
+import SwiftUI
+
+struct FirstSwiftUIGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstSwiftUIGrid()
+}
---
+++ NightWatch/Experiments/FirstSwiftUINavigationStack.swift
@@ -0,0 +1,18 @@
+//
+//  FirstSwiftUINavigationStack.swift
+//  NightWatch
+//
+//  Created by Brian Simmons on 3/18/25.
+//
+
+import SwiftUI
+
+struct FirstSwiftUINavigationStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstSwiftUINavigationStack()
+}
